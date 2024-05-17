1646b4b1628055566eee3b6dc6b9058eb102936c
wcamer <xeus.xiii@hotmail.com>
2024-05-17 16:49:35 -0600

    Oauth checks added for put post and delete routes

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
+// const { check } = require('express-validator')
 const carController = require('../controllers/carCon')
 const val = require('../util/carValidation')
+const {checkObj} = require('./auth')
 
-router.get("/" , carController.getAllCars)
+router.get("/"  ,carController.getAllCars)
 router.get("/:id", val.getCarRules(), val.getCarCheck, carController.getCar)
 // router.get("/:id",  carController.getCar)
 
-router.post("/", val.createCarRules(), val.createCarCheck, carController.createCar)
-router.put("/:id", val.updateCarRules(), val.updateCarCheck,  carController.updateCar)
-router.delete("/:id", val.deleteCarRules(), val.deleteCarCheck, carController.deleteCar)
+router.post("/", checkObj.authCheck, val.createCarRules(), val.createCarCheck, carController.createCar)
+router.put("/:id", checkObj.authCheck, val.updateCarRules(), val.updateCarCheck,  carController.updateCar)
+router.delete("/:id",checkObj.authCheck, val.deleteCarRules(), val.deleteCarCheck, carController.deleteCar)
 
 module.exports = router
--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const gameController = require('../controllers/gameCon')
 const val = require('../util/gameValidation')
+const {checkObj} = require('./auth')
 
 router.get("/" , gameController.getAllGames)
 router.get("/:id", val.getGameRules(), val.getGameCheck, gameController.getGame)
 // router.post("/",  gameController.createGame)
-router.post("/", val.createGameRules(), val.createGameCheck, gameController.createGame)
-router.put("/:id", val.updateGameRules(), val.updateGameCheck, gameController.updateGame)
-router.delete("/:id", val.deleteGameRules(), val.deleteGameCheck, gameController.deleteGame)
+router.post("/", checkObj.authCheck, val.createGameRules(), val.createGameCheck, gameController.createGame)
+router.put("/:id",  checkObj.authCheck, val.updateGameRules(), val.updateGameCheck, gameController.updateGame)
+router.delete("/:id", checkObj.authCheck, val.deleteGameRules(), val.deleteGameCheck, gameController.deleteGame)
 
 module.exports = router
